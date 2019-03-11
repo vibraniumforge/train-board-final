9cf2926595e22b51ad90869a8a7a1133288b779f
Vibraniumforge <gregm9999@gmail.com>
2019-03-10 18:33:29 -0700

    2 new routes

--- train-board-api/config/routes.rb
+++ train-board-api/config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :trains  
+      get '/amtrak-station/:id', to: "trains#amtrak_station"
+      get '/station-search/:id', to: "trains#amtrak_search"
     end
   end
 end
