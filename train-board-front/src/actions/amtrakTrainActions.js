5c02532c03d1d0412cbb5e73fff76e3bd4580b3b
Vibraniumforge <gregm9999@gmail.com>
2019-03-10 21:17:44 -0700

    cleanup

--- train-board-front/src/actions/amtrakTrainActions.js
+++ train-board-front/src/actions/amtrakTrainActions.js
@@ -1,10 +1,7 @@
 const url = "http://localhost:3001/api/v1";
 
 export const getAmtrakTrains = station => {
-  console.log(
-    "agetAmtrakTrains in amtrakTrainActions fires, station=",
-    station
-  );
+  console.log("getAmtrakTrains in amtrakTrainActions fires, station=", station);
   let data = {
     method: "GET",
     headers: {
@@ -14,25 +11,18 @@ export const getAmtrakTrains = station => {
     }
   };
   return dispatch => {
-    // fetch(
-    //   `https://cors-anywhere.herokuapp.com/http://dixielandsoftware.net/Amtrak/solari/data/${station}_schedule.php?data=${station}`,
-    //   data
-    // )
     fetch(`${url}/amtrak-station/${station}`, data)
       .then(res => res.json())
       // .then(res => console.log(res))
       .then(res =>
         dispatch({
           type: "GET_AMTRAK_TRAINS",
-          payload: res.response.results[0].data
+          payload: res.data.results[0].data
         })
       )
       .catch(err => console.error("Error in fetch=", err));
   };
 };
-
-// fetch(`${url}/trains`, data, station)
-// fetch(`https://cors-anywhere.herokuapp.com/http://dixielandsoftware.net/Amtrak/solari/data/${station.station}_schedule.php?data=${station.station}`, data)
 
 export const getAmtrakStation = station => {
   let data = {
@@ -48,10 +38,16 @@ export const getAmtrakStation = station => {
       `https://cors-anywhere.herokuapp.com/http://www.dixielandsoftware.net/cgi-bin/station_search.pl?data=${station}`,
       data
     )
-      // fetch(`${url}/station-search/${station}`, data)
       .then(res => res.text())
       // .then(res => console.log(res))
       .then(res => dispatch({ type: "GET_AMTRAK_STATION", payload: res }))
       .catch(err => console.error("Error in fetch=", err));
   };
 };
+
+// fetch(
+//   `https://cors-anywhere.herokuapp.com/http://dixielandsoftware.net/Amtrak/solari/data/${station}_schedule.php?data=${station}`,
+//   data
+// )
+
+// fetch(`http://www.dixielandsoftware.net/cgi-bin/station_search.pl?data=${station}, data)
