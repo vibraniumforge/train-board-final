38f8753f8d1a38c6df16cc2cde06881ea9c6619c
Vibraniumforge <gregm9999@gmail.com>
2019-03-10 20:22:16 -0700

    cleanup

--- train-board-front/src/actions/amtrakTrainActions.js
+++ train-board-front/src/actions/amtrakTrainActions.js
@@ -50,27 +50,8 @@ export const getAmtrakStation = station => {
     )
       // fetch(`${url}/station-search/${station}`, data)
       .then(res => res.text())
-      // .then(res => document.querySelectorAll("table").item(0))
       // .then(res => console.log(res))
       .then(res => dispatch({ type: "GET_AMTRAK_STATION", payload: res }))
       .catch(err => console.error("Error in fetch=", err));
   };
 };
-
-// document.querySelectorAll("table").item(0).textContent.trim()
-// document.getElementsByTagName("table").item(0).textContent.trim().split("\n ")
-
-// function myFx(x) {
-//   for (let i=0; i< x.length; i++)
-//   {
-//   console.log (`<a href="https://cors-anywhere.herokuapp.com/http://www.dixielandsoftware.net/cgi-bin/station_search.pl?data=${x[i].trim().slice(0,3)}" target="_blank">${x[i]}</a> <br>`)
-//   }
-//   }
-
-// myFx([ "OKJ Oakland Jack London Sq, CA ", " OAC Oakland Coliseum Airport, CA ", " ROY Royal Oak, MI ", " OKL Oakville, ON" ])
-
-// function myFx(x) {
-//   x.forEach(place=>
-//   console.log (`<a href="https://cors-anywhere.herokuapp.com/http://www.dixielandsoftware.net/cgi-bin/station_search.pl?data=${place.trim().slice(0,3)}" target="_blank">${place}</a> <br>`)
-//  )
-// }
