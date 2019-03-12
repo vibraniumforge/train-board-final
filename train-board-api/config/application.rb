8662576747039bf7074e38fe0dd79ea671bbcd6f
Vibraniumforge <gregm9999@gmail.com>
2019-03-11 21:58:29 -0700

    added patch

--- train-board-api/config/application.rb
+++ train-board-api/config/application.rb
@@ -35,7 +35,7 @@ module TrainBoardApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins 'http://localhost:3000'
-        resource '*', :headers => :any, :methods => [:get, :post, :put, :delete, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :delete, :options]
       end
     end
 
