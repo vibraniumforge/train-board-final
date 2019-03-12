1b9c06381d355d288c30ea9c209dbc4abb805076
Vibraniumforge <gregm9999@gmail.com>
2019-03-11 20:27:31 -0700

    renamed to match

---
+++ train-board-front/src/containers/ViewUserTrains.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+
+import { getUserTrains } from "../actions/userTrainActions";
+import UserBoard from "../components/UserBoard";
+
+class UserTrains extends Component {
+  componentDidMount() {
+    this.props.getUserTrains();
+  }
+
+  onSubmitTrains = e => {
+    e.preventDefault();
+    console.log("onSubmitTrains in UserTrains fires");
+    this.props.getUserTrains(() =>
+      this.props.history.push("/view_user_trains")
+    );
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+          <div className="center">
+            <h4>View my Trains:</h4>{" "}
+            <button type="button" id="trainsBtn" onClick={this.onSubmitTrains}>
+              Refresh
+            </button>
+          </div>
+        </form>
+        <UserBoard userTrains={this.props.trains} />
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  trains: state.userTrains.userSavedTrains
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(
+    {
+      getUserTrains
+    },
+    dispatch
+  );
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(UserTrains)
+);
