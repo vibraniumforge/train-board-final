d7de028c6e411b4d205d2dab4046a79f7f5a40c1
Vibraniumforge <gregm9999@gmail.com>
2019-03-12 20:59:43 -0700

    fixed update train, cleanup

--- train-board-front/src/reducers/userTrainsReducer.js
+++ train-board-front/src/reducers/userTrainsReducer.js
@@ -1,8 +1,8 @@
 const initialState = {
   userSavedTrains: [],
-  trainPostResult: [],
-  trainErrors: [],
-  trainToUpdate: []
+  trainToUpdate: [],
+  trainCreateResult: [],
+  trainErrors: []
 };
 
 export default function userTrainsReducer(state = initialState, action) {
@@ -25,7 +25,7 @@ export default function userTrainsReducer(state = initialState, action) {
       };
       return {
         ...state,
-        submittedTrain: [...state.trainPostResult, newTrain]
+        trainCreateResult: [...state.trainCreateResult, newTrain]
       };
     case "TRAIN_ERRORS":
       const errors = {
@@ -42,21 +42,21 @@ export default function userTrainsReducer(state = initialState, action) {
       return { ...state, trainErrors: [...state.trainErrors, errors] };
     case "DELETE_TRAIN":
       const userSavedTrains = state.userSavedTrains.filter(
-        train => train.id !== action.payload.data.data.id
+        train => train.id !== action.payload.data.id
       );
       return { ...state, userSavedTrains };
     case "UPDATE_TRAIN":
       const updatedTrain = {
-        id: action.payload.id,
-        destination: action.payload.destination,
-        origin: action.payload.origin,
-        remarks_boarding: action.payload.remarks_boarding,
-        scheduled: action.payload.scheduled,
-        scheduled24: action.payload.scheduled24,
-        newtime: action.payload.newtime,
-        newtime24: action.payload.newtime24,
-        service: action.payload.service,
-        trainno: action.payload.trainno
+        id: action.payload.data.id,
+        destination: action.payload.data.destination,
+        origin: action.payload.data.origin,
+        remarks_boarding: action.payload.data.remarks_boarding,
+        scheduled: action.payload.data.scheduled,
+        scheduled24: action.payload.data.scheduled24,
+        newtime: action.payload.data.newtime,
+        newtime24: action.payload.data.newtime24,
+        service: action.payload.data.service,
+        trainno: action.payload.data.trainno
       };
       return {
         ...state,
