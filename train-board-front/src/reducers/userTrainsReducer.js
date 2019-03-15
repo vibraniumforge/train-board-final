a8085cc199ff668a8f42a679b166d441a4ce5f76
Vibraniumforge <gregm9999@gmail.com>
2019-03-15 14:44:57 -0700

    refactor

--- train-board-front/src/reducers/userTrainsReducer.js
+++ train-board-front/src/reducers/userTrainsReducer.js
@@ -10,53 +10,39 @@ export default function userTrainsReducer(state = initialState, action) {
     case "GET_USER_TRAINS":
       return { ...state, userSavedTrains: action.payload };
     case "GET_TRAIN_BY_ID":
-      return { ...state, trainToUpdate: action.payload.data };
+      return { ...state, trainToUpdate: action.payload };
     case "CREATE_TRAIN":
-      const newTrain = {
-        destination: action.payload.data.destination,
-        origin: action.payload.data.origin,
-        remarks_boarding: action.payload.data.remarks_boarding,
-        scheduled: action.payload.data.scheduled,
-        scheduled24: action.payload.data.scheduled24,
-        newtime: action.payload.data.newtime,
-        newtime24: action.payload.data.newtime24,
-        service: action.payload.data.service,
-        trainno: action.payload.data.trainno
-      };
-      return {
-        ...state,
-        trainCreateResult: [...state.trainCreateResult, newTrain]
-      };
+      return { ...state, trainCreateResult: action.payload };
     case "TRAIN_ERRORS":
-      const errors = {
-        destination: action.payload.data.destination,
-        origin: action.payload.data.origin,
-        remarks_boarding: action.payload.data.remarks_boarding,
-        scheduled: action.payload.data.scheduled,
-        scheduled24: action.payload.data.scheduled24,
-        newtime: action.payload.data.newtime,
-        newtime24: action.payload.data.newtime24,
-        service: action.payload.data.service,
-        trainno: action.payload.data.trainno
-      };
-      return { ...state, trainErrors: errors };
+      // const trainErrors = {
+      //   destination: action.payload.destination,
+      //   origin: action.payload.origin,
+      //   remarks_boarding: action.payload.remarks_boarding,
+      //   scheduled: action.payload.scheduled,
+      //   scheduled24: action.payload.scheduled24,
+      //   newtime: action.payload.newtime,
+      //   newtime24: action.payload.newtime24,
+      //   service: action.payload.service,
+      //   trainno: action.payload.trainno
+      // };
+      return { ...state, trainErrors: action.payload };
     case "DELETE_TRAIN":
-      const userSavedTrains = state.userSavedTrains.filter(
-        train => train.id !== action.payload.data.id
+      const newUserSavedTrains = state.userSavedTrains.filter(
+        train => train.id !== action.payload.id
       );
-      return { ...state, userSavedTrains };
+      return { ...state, userSavedTrains: newUserSavedTrains };
     case "UPDATE_TRAIN":
       const updatedTrain = {
-        id: action.payload.data.id,
-        destination: action.payload.data.destination,
-        origin: action.payload.data.origin,
-        remarks_boarding: action.payload.data.remarks_boarding,
-        scheduled: action.payload.data.scheduled,
-        scheduled24: action.payload.data.scheduled24,
-        newtime: action.payload.data.newtime,
-        newtime24: action.payload.data.newtime24,
-        service: action.payload.data.service,
-        trainno: action.payload.data.trainno
+        id: action.payload.id,
+        destination: action.payload.destination,
+        origin: action.payload.origin,
+        remarks_boarding: action.payload.remarks_boarding,
+        scheduled: action.payload.scheduled,
+        scheduled24: action.payload.scheduled24,
+        newtime: action.payload.newtime,
+        newtime24: action.payload.newtime24,
+        service: action.payload.service,
+        trainno: action.payload.trainno
       };
       return {
         ...state,
