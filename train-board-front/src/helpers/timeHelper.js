27f4a1a9b2059fa4f7cc2d1880d7a39586c69558
Vibraniumforge <gregm9999@gmail.com>
2019-03-15 19:43:40 -0700

    refactor

--- train-board-front/src/helpers/timeHelper.js
+++ train-board-front/src/helpers/timeHelper.js
@@ -1,5 +1,5 @@
 const timeHelper = time => {
-  const newTime = !!time ? time.toString().trim() : null;
+  const newTime = time ? time.toString().trim() : null;
   // return !!newTime ? `${newTime.slice(0, 2)}:${newTime.slice(2)}` : null;
   if (newTime) {
     return `${newTime.slice(0, 2)}:${newTime.slice(2)}`;
